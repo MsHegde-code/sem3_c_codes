b0fe981a258681df794515914a1fa5479f85c187
MSH <84580747+manitshegde@users.noreply.github.com>
2021-10-29 17:59:44 +0530

    new

--- add_array_elements.c
+++ add_array_elements.c
@@ -2,7 +2,7 @@
 int main(){
     int i,arr[5]={10,20,30,40,50};
     for(i=0;i<5;i++){
-        arr[i]=arr[i+1];
+        arr[i]=arr[i]+1;
     }
     for(i=0;i<5;i++){
         printf("%d\t",arr[i]);
